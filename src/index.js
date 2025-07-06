3c08ef458ec706881054cb79fcce18519d464047
rajputnishant29 <nishantrajput7017@gmail.com>
2025-07-07 00:13:56 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from "express"
+import dotenv from "dotenv";
+import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import { app, server } from "./lib/socket.js";
+
+dotenv.config()
+// const app = express()
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+origin: [process.env.CLIENT_URL, "http://localhost:5173"],
+credentials: true
+}
+))
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+
+server.listen(PORT, ()=>{
+    console.log("it is running")
+    connectDB();
+})
